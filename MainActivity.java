40bcd6c06ddaf5505bd86449eef615ac17907b4c
gourishete <46952565+gourishete@users.noreply.github.com>
2019-01-23 12:46:10 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     view= this.getWindow().getDecorView();
+     view.setBackgroundResource(R.color.grey);
+
+    }
+}
